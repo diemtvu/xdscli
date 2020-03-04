76f451c4a56170f94498c00646c287add34cc3df
Diem Vu <diem.t.vu@gmail.com>
2020-03-03 21:28:39 -0800

    Update comments

--- xdscli.go
+++ xdscli.go
@@ -6,14 +6,14 @@
 //
 // To get LDS or CDS, use -type lds or -type cds, and provide the pod id or app label. For example:
 // ```bash
-// go run pilot_cli.go lds --proxytag httpbin-5766dd474b-2hlnx
-// go run pilot_cli.go lds --proxytag httpbin
+// go run xds.go lds --proxytag httpbin-5766dd474b-2hlnx
+// go run xds.go lds --proxytag httpbin
 // ```
 // Note If more than one pod match with the app label, one will be picked arbitrarily.
 //
 // To show only LDS with name 10.0.0.1_80
 // ```bash
-// go run pilot_cli.go lds --proxytag httpbin --name "10.0.0.1_80"
+// go run xds.go lds --proxytag httpbin -r "10.0.0.1_80"
 // ```
 //
 // For EDS/RDS, provide list of corresponding clusters or routes name. For example:
@@ -26,7 +26,7 @@
 // Script requires kube config in order to connect to k8s registry to get pod information (for LDS and CDS type). The default
 // value for kubeconfig path is .kube/config in home folder (works for Linux only). It can be changed via -kubeconfig flag.
 // ```bash
-// go run pilot_cli.go --type lds --proxytag httpbin --kubeconfig path/to/kube/config
+// go run xds.go lds --proxytag httpbin --kubeconfig path/to/kube/config
 // ```
 package main
 
